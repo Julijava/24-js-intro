436e8df2f132b9c9c5b41600e0ab9462082ada94
Julija <varnaite.j@gmail.com>
2021-01-13 14:46:11 +0200

    Initial commit

---
+++ main.js
@@ -0,0 +1,36 @@
+//vienos eilute komentaras
+
+/*
+keletos
+eiluciu
+komentaras
+*/
+
+// jei noresime ka nors issispausdinti, tai naudojame console.log()
+
+console.log(15896245);
+console.log(Labas rytas);
+
+/*
+Reiksmiu tipai:
+numer (skaicius)
+string (tekstas)
+boolem (login reiksme)
+array (sarašas)
+object (objektas)
+/*
+/*
+Kintamuju inicijavimo budai:
+ const (pagrindinis, kuri reikia rinktis by default)
+ let (renkames, jei info keiciasi)
+ var (nenaudoti)
+ */
+
+//  inicijuoti kintamaji, kuris niekada nesikeis ir bus skaicius 2021
+cost metai = 2021;
+
+// inicijuoti kintamaji, kuris niekada nesikais ir bus tekstas Petras
+const vardas = 'Petras';
+
+console.log(metai);
+console.log(vardas);
