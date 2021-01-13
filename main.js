24b315f4cd06f737c6d59b83de38567be5038c5b
Julija <varnaite.j@gmail.com>
2021-01-13 15:24:08 +0200

    Kintamieji

--- main.js
+++ main.js
@@ -18,7 +18,8 @@ string (tekstas)
 boolem (login reiksme)
 array (sarašas)
 object (objektas)
-/*
+*/
+
 /*
 Kintamuju inicijavimo budai:
  const (pagrindinis, kuri reikia rinktis by default)
@@ -27,10 +28,75 @@ Kintamuju inicijavimo budai:
  */
 
 //  inicijuoti kintamaji, kuris niekada nesikeis ir bus skaicius 2021
-cost metai = 2021;
+const metai = 2021;
 
 // inicijuoti kintamaji, kuris niekada nesikais ir bus tekstas Petras
 const vardas = 'Petras';
 
 console.log(metai);
 console.log(vardas);
+
+console.log(metai, vardas);
+
+console.log('Metai:', metai);
+console.log('Asmens vardas:', vardas, 'ir jis gyveno:', metai, metais.');
+
+const pirmasSkaicius = 13;
+const antrasSkaicius = 2;
+
+const suma = pirmasSkaicius + antrasSkaicius;
+console.log(suma);
+console.log(pirmasSkaicius, '+', antrasSkaicius, '=', suma);
+
+let adresas = 'Vilnius';
+console.log(adresas);
+
+adresas = 'Kaunas';
+console.log(adresas);
+
+adresas = 'Klaipedos svyturys';
+console.log(adresas);
+
+let pinigine = 0;
+console.log('Turiu pinigu:', pinigine);
+
+const pirmaAlga = 100;
+pinigine = pinigine + pirmaAlga;
+console.log('Turiu pinigu:', pinigine);
+
+const pirmaAlga = 250;
+pinigine = pinigine + pirmaAlga;
+console.log('Turiu pinigu:', pinigine);
+
+const pirmaAlga = 650;
+pinigine = pinigine + pirmaAlga;
+console.log('Turiu pinigu:', pinigine);
+
+console.log('');
+
+//  matematines operacijos
+const s1 = 5;
+const s2 = 8;
+
+console.log(s1 + s2);
+
+console.log(s1 + s2);
+console.log(s1 -s2);
+console.log(s1 * s2);
+console.log(s1 / s2);
+console.log(s1 ** s2); // kelimas laipsniu: penki astuntuoju
+
+console.log(2 + 2 * 2);
+console.log((2 +2) * 2);
+
+console.log'-----------------------');
+
+//tekstines operacijos
+
+const hi = 'Labas';
+const morning = 'rytas';
+
+const sayHi = hi + ' ' + morning + '!';
+console.log(sayHi)
+
+
