962d89119a6dc07ec700c2b8bd86e0516ad1f69c
Julija <varnaite.j@gmail.com>
2021-01-13 16:56:47 +0200

    Array and length

---
+++ array.js
@@ -0,0 +1,29 @@
+/*
+Array - bet kokiu elementu sarasas (prioritetas, to paties tipo elementai)
+*/
+
+const tusciaPinigine = [];
+console.log(tusciaPinigine);
+
+const pinigine = [1, 2, 5, 5, 20];
+console.log(pinigine);
+
+const abc ['a', 'b', 'c', 'd', 'e', 'f', 'g'];
+console.log(abc);
+
+const aplankytiMiestai = ['Vilnius', 'Kaunas', 'Klaipeda'];
+console.log(aplankytiMiestai);
+
+console.log('Kiek yra reiksmiu tusciame sarase:', tusciaPinigine.length);
+console.log('Kiek yra reiksmiu pinigineje:', pinigine.length);;
+console.log('Kiek reiksmiu yra abeceleje:', abc.length);
+console.log('Kiek esu aplankes miestu:', aplankytiMiestai.length);
+
+//kaip suzinoti koks teksto ilgis: tekstas.length
+
+const zodis = 'zodis';
+const zodisIlgis = zodis.length;
+console.log('"${zodis} ilgis yra ${zodisIlgis}';
+)
+
+ 
